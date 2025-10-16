ae9de40dec977e8d479ab08f51c6370063f0e981
sanjana9560 <121418058+sanjana9560@users.noreply.github.com>
2025-10-16 18:55:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,19 @@ const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+    <header>
+     <h1>Welcom to my website</h1>
+    </header>
+    <footer>
+    <h4>This is the content of my website</h4>
+    </footer>
+    <div>
+    <h4>
+    C 2023 My Website. All rights reserved
+    </h4>
+    </div>
+    
+   
     </div>
   )
 }
