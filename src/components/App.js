df21602dd6c81fab52ff01017643fc40a9200091
sanjana9560 <121418058+sanjana9560@users.noreply.github.com>
2025-10-23 23:48:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
-
 import React from "react";
 import './../styles/App.css';
+
+const PageLayout = ({ header, footer, children }) => {
+  return (
+    <div>
+      {header}
+      <div>{children}</div>
+      {footer}
+    </div>
+  );
+};
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    <header>
-     <h1>Welcom to my website</h1>
-    </header>
-    <footer>
-    <h4>This is the content of my website</h4>
-    </footer>
-    <div>
-    <h4>
-    C 2023 My Website. All rights reserved
-    </h4>
+      {/* Do not remove the main div */}
+      <PageLayout
+        header={<header><h1>Welcome to my website</h1></header>}
+        footer={<footer><h4>C 2023 My Website. All rights reserved</h4></footer>}
+      >
+        <h4>This is the content of my website</h4>
+      </PageLayout>
     </div>
-    
-   
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
